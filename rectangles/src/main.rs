7262bcd70b7654405c388d8cfd66bee333eee67d
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-06-17 11:10:38 -0400

    converted area to be a method instead of a function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,16 +3,18 @@ struct Rectangle {
     width: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         height: 30,
         width: 50
     };
 
-    println!("The area of the rectangle is: {}", area(&rect1));
+    println!("The area of the rectangle is: {}", rect1.area());
 
-}
-
-fn area(rect: &Rectangle) -> u32{
-    rect.height * rect.width
 }
